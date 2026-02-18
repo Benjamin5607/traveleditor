81c073342b0a158e211004000200eab3c9cf8fd1
Benjamin <86463042+Benjamin5607@users.noreply.github.com>
2026-02-18 23:44:34 +0800

    Update next.config.ts

--- emily-traveleditor/next.config.ts
+++ emily-traveleditor/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/traveleditor',
-  trailingSlash: true, // 경로 끝에 / 붙여서 인식률 높임
+  trailingSlash: true,
   images: { unoptimized: true },
 };
 
